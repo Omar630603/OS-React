34bfa5d2eb0f7cc8feb4964c2c2f4cc090a7c377
Omar Al-Maktary <omar.yem1111@gmail.com>
2021-11-26 12:20:20 +0700

    remove log from the code

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Omar Al-Maktary <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Omar Al-Maktary</p>
         <a
           className="App-link"
           href="https://reactjs.org"
